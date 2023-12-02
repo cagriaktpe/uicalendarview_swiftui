cf52980771c56645dd7df489a0ca28967465f9ee
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-12-02 09:37:36 +0300

    CalendarView

--- UICalendarView_SwiftUI/Views/CalendarViewTab/CalendarView.swift
+++ UICalendarView_SwiftUI/Views/CalendarViewTab/CalendarView.swift
@@ -13,16 +13,46 @@ struct CalendarView: UIViewRepresentable {
     
     func makeUIView(context: Context) -> UICalendarView {
         let view = UICalendarView()
+        view.delegate = context.coordinator
         view.calendar = Calendar(identifier: .gregorian)
         view.availableDateRange = interval
         return view
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self, eventStore: _eventStore)
     }
     
     func updateUIView(_ uiView: UICalendarView, context: Context) {
         
     }
     
-    
-   
-    
+    class Coordinator: NSObject, UICalendarViewDelegate {
+        var parent: CalendarView
+        @ObservedObject var eventStore: EventStore
+        
+        init(parent: CalendarView, eventStore: ObservedObject<EventStore>) {
+            self.parent = parent
+            self._eventStore = eventStore
+        }
+        
+        @MainActor
+        func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
+            let foundEvents = eventStore.events
+                .filter { $0.date.startOfDay == dateComponents.date?.startOfDay }
+            if foundEvents.isEmpty {
+                return nil
+            }
+            if foundEvents.count > 1 {
+                return .image(UIImage(systemName: "doc.on.doc.fill"), color: .red, size: .large)
+            }
+            let singleEvent = foundEvents.first!
+            return .customView {
+                let icon = UILabel()
+                icon.text = singleEvent.eventType.icon
+                return icon
+            }
+        }
+    }
+
 }
